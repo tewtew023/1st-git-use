8208b022ee86844ef2a245e039c0a995728da9c8
Phatsara003 <phatsara_ae@hotmail.com>
2018-04-10 17:42:45 +0700

    noobgg

--- Gittest1/src/gittest1/Gittest1.java
+++ Gittest1/src/gittest1/Gittest1.java
@@ -9,13 +9,25 @@ package gittest1;
  *
  * @author STD
  */
-public class Gittest1 {
+
+class noob{
+     int a;
+    public void noob(){
+        System.out.println("nooooobbbbbbbbbbbbbbbb");
+    }
+}
+
+
+
+class Gittest1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Moiza and his friends Github test!");
+        System.out.println("Alosirise");
+        
     }
     
 }
