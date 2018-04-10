5cf7aa0245288758b708ebac6a529d2d7bdf7d3c
Phatsara003 <phatsara_ae@hotmail.com>
2018-04-10 17:49:03 +0700

    noobgg

--- Gittest1/src/gittest1/Gittest1.java
+++ Gittest1/src/gittest1/Gittest1.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package gittest1;
 
-/**
- *
- * @author STD
- */
-
 class noob{
-     int a;
+    int a;
     public void noob(){
         System.out.println("nooooobbbbbbbbbbbbbbbb");
     }
@@ -25,14 +17,13 @@ class door{
 
 
 class Gittest1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Moiza and his friends Github test!");
         System.out.println("Alosirise");
-        
+        System.out.println("Alivezero");
+        System.out.println("");
+        door a = new door();
+        a.bewinw();
     }
     
 }
