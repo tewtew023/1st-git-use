7198c9d951f0f12cbb22bdef975e0ae24898a289
11 <g>
2018-04-10 17:36:07 +0700

    Moi master Commit

--- Gittest1/src/gittest1/Gittest1.java
+++ Gittest1/src/gittest1/Gittest1.java
@@ -15,7 +15,7 @@ public class Gittest1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Moiza and his friends Github test!");
     }
     
 }
